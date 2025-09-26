c4a6f59650a971e0ea672e6d806eebef2265a1eb
Alessandro Molina <alessandro@molina.fyi>
2025-09-26 17:32:37 +0200

    Replace hardcoded GitHub token with environment variable

--- newsletter.py
+++ newsletter.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 
 # Expires after 1 year as ASF forbids access to their repositories
 # with endless tokens.
-GITHUB_API_TOKEN = "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"
+GITHUB_API_TOKEN = os.environ["TOKEN_API_GITHUB"]
 GITHUB_RELEASES_API = "https://api.github.com/repos/{ORG}/{REPO}/releases"
 NEWSLETTER_NEWS_md = "https://api.github.com/repos/amol-/modern-datascience/contents/NEWS.md"
 NOW = datetime.datetime.utcnow()
